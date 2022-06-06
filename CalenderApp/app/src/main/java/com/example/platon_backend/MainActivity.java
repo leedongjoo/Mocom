3bb65e20690105ca86b4869a793d3ab39667c24f
leedongjoo <dong2sul@naver.com>
2022-06-07 03:49:03 +0900

    일단 안드로이드 스튜디오 예제 변수만 다바꿔줌

--- CalenderApp/app/src/main/java/com/example/platon_backend/MainActivity.java
+++ CalenderApp/app/src/main/java/com/example/platon_backend/MainActivity.java
@@ -4,18 +4,11 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.compose.ui.text.intl.Locale;
 
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import java.io.BufferedReader;
 
@@ -25,18 +18,17 @@ import java.io.OutputStream;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String IP_ADDRESS = "34.64.113.235";
+    private static String IP_ADDRESS = "34.64.224.190";
     private static String TAG = "Server Interworking";
 
+    private EditText month;
+    private EditText day;
     private EditText name;
-    private EditText code;
-    private EditText division;
-    private EditText phoneNo;
-    private EditText address;
+    private EditText contents;
+
 //    private EditText mEditTextbat_level;
 //    private TextView mTextViewResult;
 
@@ -46,31 +38,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText)findViewById(R.id.temp1);
-        code = (EditText)findViewById(R.id.temp5);
-        division = (EditText)findViewById(R.id.temp2);
-        phoneNo = (EditText)findViewById(R.id.temp3);
-        address = (EditText)findViewById(R.id.temp4);
+        month = (EditText)findViewById(R.id.temp1);
+        day = (EditText)findViewById(R.id.temp5);
+        name = (EditText)findViewById(R.id.temp2);
+        contents = (EditText)findViewById(R.id.temp3);
 
         Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { // 클릭시 전송.
 
-                String name_s = name.getText().toString();
-                String code_s = code.getText().toString();
-                String division_s = division.getText().toString();
-                String phoneNo_s = phoneNo.getText().toString();
-                String address_s = address.getText().toString();
+                String name_s = month.getText().toString();
+                String code_s = day.getText().toString();
+                String division_s = name.getText().toString();
+                String phoneNo_s = contents.getText().toString();
 
                 InsertData task = new InsertData();
-                task.execute("http://" + IP_ADDRESS + "/insert_student.php", name_s, code_s, division_s, phoneNo_s, address_s);
+                task.execute("http://" + IP_ADDRESS + "/insert_student.php", name_s, code_s, division_s, phoneNo_s);
 
+                month.setText("");
+                day.setText("");
                 name.setText("");
-                code.setText("");
-                division.setText("");
-                phoneNo.setText("");
-                address.setText("");
+                contents.setText("");
             }
         });
 
@@ -101,16 +90,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
 
-            String name_s = (String)params[1];
-            String code_s = (String)params[2];
-            String division_s = (String)params[3];
-            String phoneNo_s = (String)params[4];
-            String address_s = (String)params[5];
+            String month_s = (String)params[1];
+            String day_s = (String)params[2];
+            String name_s = (String)params[3];
+            String contents_s = (String)params[4];
 
 
             String serverURL = (String)params[0];
-            String postParameters = "name_s=" + name_s + "&code_s=" + code_s + "&division_s="
-                    + division_s + "&phoneNo_s=" + phoneNo_s + "&address_s=" + address_s;
+            String postParameters = "name_s=" + month_s + "&day_s=" + day_s + "&name_s="
+                    + name_s + "&contents_s=" + contents_s;
 
 
             try {
